27a5386c01bee08749b76d5fc6398a1e9050f64f
emity <emityoffwhite@gmail.com>
2026-03-27 19:42:09 +0300

    simple_sql

--- feature_postgres/simple_connection/simple_connection.go
+++ feature_postgres/simple_connection/simple_connection.go
@@ -2,21 +2,11 @@ package simple_connection
 
 import (
 	"context"
-	"fmt"
 	"github.com/jackc/pgx/v5"
 )
 
-func CheckConnection() {
+func CreateConnection(ctx context.Context) (*pgx.Conn, error) {
 
-	ctx := context.Background()
+	return pgx.Connect(ctx, "postgres://postgres:123@localhost:5432/postgres")
 
-	conn, err := pgx.Connect(ctx, "postgres://postgres:123@localhost:5432/postgres")
-	if err != nil {
-		panic(err)
-	}
-
-	if err := conn.Ping(ctx); err != nil { // вызываем Ping() с контекстом
-		panic(err)
-	}
-	fmt.Println("Подключение к базе данных произошло успешно!")
 }
